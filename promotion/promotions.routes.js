b3616e85244ce90e23f6ad0c6823c0dceaedf85b
Navediqbal123 <navedahmad9012@gmail.com>
2025-11-27 20:57:21 +0530

    Fix import path for promotions.controller

--- promotion/promotions.routes.js
+++ promotion/promotions.routes.js
@@ -1,7 +1,10 @@
 const express = require("express");
 const router = express.Router();
-const controller = require("./promotions.controller");
 
+// IMPORTANT FIXED PATH
+const controller = require("../promotion/promotions.controller.js");
+
+// Routes
 router.post("/create", controller.createPromotion);
 router.get("/active", controller.getActivePromotions);
 router.put("/toggle/:id", controller.togglePromotion);
