1800d2331d8703b623c2fdf9fbd9250d02bcdf83
Navediqbal123 <navedahmad9012@gmail.com>
2025-11-27 21:12:04 +0530

    Refactor promotion controller to use named exports

--- promotion/promotions.controller.js
+++ promotion/promotions.controller.js
@@ -1,16 +1,22 @@
 const service = require("./promotions.service");
 
-exports.createPromotion = async (req, res) => {
+const createPromotion = async (req, res) => {
   const result = await service.createPromotion(req.body);
   res.json(result);
 };
 
-exports.getActivePromotions = async (req, res) => {
+const getActivePromotions = async (req, res) => {
   const result = await service.getActivePromotions();
   res.json(result);
 };
 
-exports.togglePromotion = async (req, res) => {
+const togglePromotion = async (req, res) => {
   const result = await service.togglePromotion(req.params.id);
   res.json(result);
+};
+
+module.exports = {
+  createPromotion,
+  getActivePromotions,
+  togglePromotion
 };
