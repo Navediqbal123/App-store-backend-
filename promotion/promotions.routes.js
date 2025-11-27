820a4cc32f75e6132031b62e5d943b61443a5022
Navediqbal123 <navedahmad9012@gmail.com>
2025-11-27 22:16:54 +0530

    Update promotions.routes.js

--- promotion/promotions.routes.js
+++ promotion/promotions.routes.js
@@ -1,10 +1,8 @@
 const express = require("express");
 const router = express.Router();
 
-// IMPORTANT FIXED PATH
-const controller = require("../promotion/promotions.controller.js");
+const controller = require("./promotions.controller.js");
 
-// Routes
 router.post("/create", controller.createPromotion);
 router.get("/active", controller.getActivePromotions);
 router.put("/toggle/:id", controller.togglePromotion);
